d3a0e2b6e31b24599e39ac9d9bcc7b8a0ce78030
Adarsh Kumar <2020kucp1016@iiitkota.ac.in>
2023-07-12 22:10:55 +0530

    navbar1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/navbar";
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from './components/Home';
+import Signup from './components/signup';
+import Signin from './components/signin';
+import Profile from './components/profile';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/signin" element={<Signin/>}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
---
+++ src/components/navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from "../img/logo.png"
+import "./navbar.css"
+import {Link} from "react-router-dom"
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <img src={logo} alt="" />
+            <ul className='nav-menu'>
+                <Link to="/signup"><li>Sign Up</li></Link>
+                <Link to="/signin"><li>Sign In</li></Link>
+                <Link to="/profile"><li>Profile</li></Link>
+                
+                
+                
+
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
