5260addd11d326c512c6e8039a60d1bdecfe4a6b
Adarsh Kumar <2020kucp1016@iiitkota.ac.in>
2023-07-16 14:06:52 +0530

    cloudinary

--- src/components/Createpost.js
+++ src/components/Createpost.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Createpost.css"
+import { toast } from 'react-toastify';
+import { useNavigate } from "react-router-dom"
 
 function Createpost() {
+
+    const [body, setBody] = useState("");
+    const [image, setImage] = useState("");
+    const [url, setUrl] = useState("");
+    const navigate = useNavigate()
+
+    const notifyA = (msg) => toast.error(msg)
+    const notifyB = (msg) => toast.success(msg)
+
+    useEffect(() => {
+        //Saving in mongodb
+        if (url) {
+            fetch("http://localhost:5000/createPost", {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + localStorage.getItem("jwt")
+                },
+                body: JSON.stringify({
+                    body,
+                    pic: url
+                })
+            }).then(res => res.json())
+                .then(data => {if(data.error){
+                    notifyA(data.error)
+                }else{
+                    notifyB("Successfully Posted")
+                    navigate("/")
+                }})
+                .catch(err => console.log(err))
+
+        }
+
+
+
+    }, [url])
+
+    //Cloudinary
+    const postDetails = () => {
+
+
+        console.log(body, image)
+        const data = new FormData()
+        data.append("file", image)
+        data.append("upload_preset", "insta-clone")
+        data.append("cloud_name", "adarshmi")
+        fetch("https://api.cloudinary.com/v1_1/adarshmi/image/upload", {
+            method: "post",
+            body: data
+        }).then(res => res.json())
+            .then(data => setUrl(data.url))
+            .catch(err => console.log(err))
+        console.log(url)
+
+
+    }
 
     const loadfile = (event) => {
         var output = document.getElementById('output');
@@ -11,29 +69,34 @@ function Createpost() {
 
         };
     };
-        return (
+    return (
 
-            < div className='createPost' >
-                <div className="post-header">
-                    <h4 style={{ margin: "3px auto" }}>Create New Post</h4>
-                    <button id="post-btn">Share</button>
-                </div>
-                <div className="main-div">
-                    <img id="output" src="https://pixsector.com/cache/517d8be6/av5c8336583e291842624.png"/>
-                    <input type="file" accept="image/*"  onChange={(event)=>{loadfile(event)}}/>
-                </div>
-                <div className="details">
-                    <div className="card-header">
-                        <div className="card-pic">
-                            <img src="https://images.unsplash.com/photo-1595152452543-e5fc28ebc2b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjA0fHxmYWNlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt='' />
-                        </div>
-                        <h5>Ankur</h5>
+        < div className='createPost' >
+            <div className="post-header">
+                <h4 style={{ margin: "3px auto" }}>Create New Post</h4>
+                <button id="post-btn" onClick={() => { postDetails() }}>Share</button>
+            </div>
+            <div className="main-div">
+                <img id="output" src="https://pixsector.com/cache/517d8be6/av5c8336583e291842624.png" />
+                <input type="file" accept="image/*" onChange={(event) => {
+                    loadfile(event);
+                    setImage(event.target.files[0])
+                }} />
+            </div>
+            <div className="details">
+                <div className="card-header">
+                    <div className="card-pic">
+                        <img src="https://images.unsplash.com/photo-1595152452543-e5fc28ebc2b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjA0fHxmYWNlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt='' />
                     </div>
-                    <textarea type="text" placeholder="Write a caption..."></textarea>
+                    <h5>Ankur</h5>
                 </div>
-            </div >
-        )
+                <textarea value={body} onChange={(e) => {
+                    setBody(e.target.value)
+                }} type="text" placeholder="Write a caption..."></textarea>
+            </div>
+        </div >
+    )
 
-    }
+}
 
-    export default Createpost;
+export default Createpost;
--- src/components/signin.js
+++ src/components/signin.js
@@ -1,47 +1,50 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './signin.css'
 import logo from "../img/logo.png"
-import { Link,useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { toast } from 'react-toastify';
 
 
 function Signin() {
     const navigate = useNavigate()
-    const [email,setEmail]=useState("")
-    const [password,setPassword]=useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
     const notifyA = (msg) => toast.error(msg)
     const notifyB = (msg) => toast.success(msg)
 
     const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    const postData=()=>{
-        if(!emailRegex.test(email)){
+    const postData = () => {
+        if (!emailRegex.test(email)) {
             notifyA("Invalid Email")
             return
         }
 
 
-        fetch("http://localhost:5000/signin",{
-            method:"post",
-            headers:{
-                "Content-Type":"application/json"
+        fetch("http://localhost:5000/signin", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
             },
-            body:JSON.stringify({
-                email:email,
-                password:password
+            body: JSON.stringify({
+                email: email,
+                password: password
             })
-        }).then(res=>res.json())
-        .then(data => {
-            if(data.error){
-                notifyA(data.error)
-            }else{
-                notifyB(data.message)
-                navigate("/")
-            }
-            
-            console.log(data)})
-        
+        }).then(res => res.json())
+            .then(data => {
+                if (data.error) {
+                    notifyA(data.error)
+                } else {
+                    notifyB(data.message)
+                    console.log(data)
+                    localStorage.setItem("jwt", data)
+                    navigate("/")
+                }
+
+                console.log(data)
+            })
+
     }
 
 
@@ -51,16 +54,16 @@ function Signin() {
                 <div className='loginForm'>
                     <img className="Signup_logo" src={logo} alt="" />
                     <div>
-                    <input type="email" name='email' id="email" value={email} placeholder='Email' onChange={(e)=>{setEmail(e.target.value)}} />
+                        <input type="email" name='email' id="email" value={email} placeholder='Email' onChange={(e) => { setEmail(e.target.value) }} />
                     </div>
                     <div>
-                    <input type="password" name='password' id="password" placeholder='Password' value={password} onChange={(e)=>{setPassword(e.target.value)}} />
+                        <input type="password" name='password' id="password" placeholder='Password' value={password} onChange={(e) => { setPassword(e.target.value) }} />
                     </div>
-                    <input type="submit" id="login-btn" onClick={()=>{postData()}} value="Sign In" />
+                    <input type="submit" id="login-btn" onClick={() => { postData() }} value="Sign In" />
                 </div>
                 <div className='loginForm2'>
-                    Don't have an account ?<Link to="/signup"> 
-                    <span style={{ color: "blue", cursor: "pointer" }}>Sign Up</span></Link>
+                    Don't have an account ?<Link to="/signup">
+                        <span style={{ color: "blue", cursor: "pointer" }}>Sign Up</span></Link>
 
                 </div>
             </div>
